1a13557107f8e34d8667c0de173b70c2e8b53512
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-11-15 19:05:21 +0530

    refactor : changed code formatting
    changed code formatting for better readability and snapped all the var declarations for let and const

--- index.js
+++ index.js
@@ -1,32 +1,67 @@
-const readlineSync = require("readline-sync");
+const readlineSync = require("readline-sync")
 const chalk = require("chalk")
-let score = 0;
+let score = 0
 let userWelcome = () => {
-  var userName = readlineSync.question ("What is your name? ")
+  const userName = readlineSync.question("What is your name? ")
   console.log(chalk.blue("Welcome to the game, ") + chalk.red(userName))
-  console.log ("Lets play a Marvel Quiz")
-  console.log (" ")
-  console.log ("If you answers all correctly, You will go to Next Level. " + "Here goes the first question")
-  console.log (" ")
+  console.log("Lets play a Marvel Quiz")
+  console.log(" ")
+  console.log("If you answers all correctly, You will go to Next Level. " + "Here goes the first question")
+  console.log(" ")
 }
 
 
-var questions = [
-  {question: "What is inside the Eye of Agamoto? ",
-  answer: "Time stone"},
-  {question: "How many Infinity Stones are there? ",
-  answer: "6"},
-  {question: "Who was responsible for King T’Chaka’s death? ",
-  answer: "Zemo"},
-  {question: "On what planet was the Soul Stone in Infinity War? ",
-  answer: "Vormir"},
-  {question: "Captain America’s shield and Bucky’s arm are made of what? ",
-  answer: "Vibranium"},
-  {question: "Sharon Carter is whose great-niece? ",
-  answer: "Peggy Carter"},
-  {question: "Whose power exceeds that of the Sorcerer Supreme? ",
-  answer: "Scarlet Witch"}
+let scoreTracker = () => {
+
+  console.log("and your total score is: " + score)
+  if (score === 7) {
+    console.log("Yay! you are going to next level")
+  } else {
+    console.log("Sorry you didn't clear this level")
+  }
+
+  let highScore = {
+    name: "Debasish Sahoo",
+    score: 7
+  }
+
+  console.log(chalk.inverse("Compare yourself with Highest Scorer: "))
+  console.log(chalk.inverse(highScore.name + "-" + highScore.score))
+  
+}
+
+
+const questionDatabase = [
+  {
+    question: "What is inside the Eye of Agamoto? ",
+    answer: "Time stone"
+  },
+  {
+    question: "How many Infinity Stones are there? ",
+    answer: "6"
+  },
+  {
+    question: "Who was responsible for King T’Chaka’s death? ",
+    answer: "Zemo"
+  },
+  {
+    question: "On what planet was the Soul Stone in Infinity War? ",
+    answer: "Vormir"
+  },
+  {
+    question: "Captain America’s shield and Bucky’s arm are made of what? ",
+    answer: "Vibranium"
+  },
+  {
+    question: "Sharon Carter is whose great-niece? ",
+    answer: "Peggy Carter"
+  },
+  {
+    question: "Whose power exceeds that of the Sorcerer Supreme? ",
+    answer: "Scarlet Witch"
+  }
 ]
+
 
 userWelcome()
 
@@ -43,19 +78,4 @@ questionDatabase.map((DB) => {
   console.log("--------------------")
 })
 
-
-
-console.log ("and your total score is: " + score)
-if (score === 7){
-  console.log ("Yay! you are going to next level")
-} else {
-  console.log ("Sorry you didn't clear this level")
-}
-
-var highScore = {
-  name: "Debasish Sahoo",
-  score: 7
-}
-
-console.log (chalk.inverse("Compare yourself with Highest Scorer: "))
-console.log (chalk.inverse(highScore.name + "-" +  highScore.score))
+scoreTracker()
